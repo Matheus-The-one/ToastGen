abcb135852f86b7804bff7447f97a9f2326dd628
matheustheone <matepapava123@gmail.com>
2024-07-01 23:51:57 +0400

    adding pages folder and the toastorder page

--- toastgen/app/pages/ToastOrder.tsx
+++ toastgen/app/pages/ToastOrder.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Lists = () => {
+const ToastOrder = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Lists = () => {
   )
 }
 
-export default Lists
+export default ToastOrder
