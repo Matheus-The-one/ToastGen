c6a23b3b147156470f029c77eec277f4c8378ecd
matheustheone <matepapava123@gmail.com>
2024-07-08 00:49:02 +0400

    .

--- toastgen/app/(pages)/toastorder/page.tsx
+++ toastgen/app/(pages)/toastorder/page.tsx
@@ -1,20 +1,18 @@
-
 import { TableDemo } from '@/app/components/Table'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div className="min-h-screen bg-purple-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gradient-to-br from-purple-900 to-black py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
-        <h1 className="text-3xl font-extrabold text-purple-900 mb-8 text-center">
-          Your Table Page
+        <h1 className="text-4xl font-extrabold text-white mb-8 text-center">
+          ლხინის სუფრის სადღეგრძელოების ჩამონათვალი
         </h1>
-        <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+        <div className="bg-black text-white shadow-2xl rounded-lg overflow-hidden backdrop-blur-sm">
           <TableDemo />
         </div>
       </div>
     </div>
   )
 }
-
 export default Page
